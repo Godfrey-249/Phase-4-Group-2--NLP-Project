6ba7409204fbf4768e5b78a5b5f7a44faaec034e
Muthonikiboi <kiboijoye254@gmail.com>
2025-07-15 13:47:33 +0300

    ADD: Descriptions and Minor Changes

--- Skemers.py
+++ Skemers.py
@@ -1,32 +1,3 @@
-# # importing libraries
-# import streamlit as st
-# import joblib
-
-# #Loading the model 
-# vectorizer, model = joblib.load("Logistic_model.pkl")
-
-# # Creating the app
-# st.write("# Twitter sentiment analysis app")
-# st.write("## Hello, fill in a tweet and you will get a sentiment")
-
-
-# def predict_sentiment(scenario):
-#     # Clean and preprocess the input if needed
-#     processed = vectorizer.transform([scenario])
-#     prediction = model.predict(processed)
-    
-#     if prediction[0] == 1:
-#         print('Positive')
-#     elif prediction[0] == -1:
-#         print('Negative')
-#     else:
-#         print('Neutral')
-
-# #Sentiment outputs
-# st.text_input("Type the Tweet below")
-# scenario = st.text_input("Type the Tweet below")
-# st.write(f"This tweet is: {predict_sentiment(scenario)}")
-
 # importing libraries
 import streamlit as st
 import joblib
